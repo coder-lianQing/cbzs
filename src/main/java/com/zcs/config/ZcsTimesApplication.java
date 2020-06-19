45249a0888e6b4533641ba213d2c4e7523cb9d75
lyj <liuyujingchan@163.com>
2020-06-19 11:53:21 +0800

    zcsTimes

---
+++ src/main/java/com/zcs/config/ZcsTimesApplication.java
@@ -0,0 +1,17 @@
+package com.zcs.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+* 项目启动类
+* @author Legion
+* 2019-07-03 20:12:32
+*/
+@SpringBootApplication
+public class ZcsTimesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZcsTimesApplication.class, args);
+    }
+}
---
+++ src/test/java/com/zcs/config/ZcsTimesApplicationTest.java
@@ -0,0 +1,14 @@
+package com.zcs.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+
+@Slf4j
+public class ZcsTimesApplicationTest {
+
+    @Test
+    public void app(){
+        log.info("Test data.");
+    }
+}
